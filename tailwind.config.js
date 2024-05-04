94aaa97f180205e2f6164e98ec4bc4bf0a3533e1
Morteza Gholamzade <mor.gholami.1@gmail.com>
2024-05-04 17:11:51 +0430

    add content to page1

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
       colors: {
         'secondary-color': '#232441',
         'light-color': '#F0F3FF',
+        'light-color-2': '#52559F',
+        'light-color-3': '#DDE4FE',
         'primary-color': '#E7FE58',
         'light-3': '#6A7CBC',
         
@@ -13,6 +15,13 @@ module.exports = {
 
       height:{
         '278': '1112px',
+        '18': '74px',
+        '66': '264px',
+
+      },
+      width:{
+        '26': '98px',
+        '66': '264px',
 
       },
       borderRadius:{
@@ -20,6 +29,10 @@ module.exports = {
       },
       boxShadow: {
         '3xl': '0 -1px 20px  rgba(0, 0, 0, 0.2)',
+        '4xl': '0px 4px 20px 0px rgba(0, 0, 0, 0.11)',
+        '5xl': '0px 3px 20px 0px rgba(11, 77, 82, 0.18)'
+
+
       }
     },
   },
